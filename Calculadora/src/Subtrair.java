56b0f3378c1e5e189c0e70eebbbd6d2d027bdaff
System Administrator <root@Victors-MacBook-Pro.local>
2018-11-07 01:40:03 -0200

    Exercicio usando JFrame

---
+++ Calculadora/src/Subtrair.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victorhugo
+ */
+public class Subtrair extends Matematica {
+    int n1, n2, resultado;
+ @Override
+ 
+    public int calcular(){
+    
+        resultado = n1 - n2;
+    
+    return (resultado);
+    }
+}
