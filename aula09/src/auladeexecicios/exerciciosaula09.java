3750a720ec4eabdef7725cad23db0a649c87e9eb
System Administrator <root@Victors-MacBook-Pro.local>
2018-11-07 01:15:57 -0200

    Nona aula de Programação orientada à objetos

---
+++ aula09/src/auladeexecicios/exerciciosaula09.java
@@ -0,0 +1,98 @@
+package auladeexecicios;
+import java.util.Scanner;
+public class exerciciosaula09 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner (System.in);
+//-------------------------------------------------------------------------------------------------------------
+//  Escreva um algoritmo para ler 2 valores e, se o valor informado for ZERO, deve ser lido um novo valor
+//  ou seja, para o segundo valor não pode ser aceito o valor zero e imprimir o resultado da divisão do primeiro valor 
+//  lido pelo segundo valor lido.        
+//-------------------------------------------------------------------------------------------------------------
+//  int x,y,result;
+//            
+//            System.out.println("Informe um valor: ");
+//            x = leitor.nextInt();
+//    do{
+//        
+//        System.out.println("Informe outro valor: ");
+//            y= leitor.nextInt();
+//    }while(y == 0);
+//    
+//    result = x / y;
+//    
+//        System.out.printf("%d / %d = %d\n",x,y,result);
+//    
+//-------------------------------------------------------------------------------------------------------------
+// Acrescentar uma mensagem de 'VALOR INVÁLIDO' no exercício 2 caso o segundo valor informado seja ZERO.
+//-------------------------------------------------------------------------------------------------------------
+//int x, y;
+//
+//        System.out.println("informeum valor inteiro: ");
+//        x = leitor.nextInt();
+//        
+//        do{
+//            System.out.println("Informe outro valor");
+//            y = leitor.nextInt();
+//            
+//            if(y==0){
+//                System.out.println("Informe um valor diferente de ZERO");
+//            }
+//        }while(y==0);
+//-------------------------------------------------------------------------------------------------------------
+// Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente.
+//-------------------------------------------------------------------------------------------------------------
+//int x=1;
+//do{
+//    System.out.println(x);
+//x++;
+//}while(x<=10);
+//-------------------------------------------------------------------------------------------------------------
+// Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
+//-------------------------------------------------------------------------------------------------------------
+//int x=10;
+//do{
+//    System.out.println(x);
+//    x--;
+//}while(x>=1);
+//-------------------------------------------------------------------------------------------------------------        
+// Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100.
+//-------------------------------------------------------------------------------------------------------------
+//int x=1;
+//
+//do{
+//    x++;
+//    if(x > 100 && x <= 110){
+//        System.out.println(x);
+//    }
+//}while(x<=110);
+
+//Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive). Considere que o N será sempre maior que ZERO.
+
+//int n,cont=1;
+//        
+//                System.out.println("Informe um valor para N");
+//                n = leitor.nextInt();
+//        do{
+//            System.out.println(cont);
+//            cont++; 
+//        }while(n>=cont);
+//-------------------------------------------------------------------------------------------------------------
+// Ler um valor inteiro (aceitar somente valores entre 1 e 10) e escrever a tabuada de 1 a 10 do valor lido.
+//-------------------------------------------------------------------------------------------------------------
+//int r, x,tabuada=1;
+//
+//        System.out.println("Infome um numero para cunsultar a tabuada do 1 ao 10: ");
+//        x = leitor.nextInt();
+//        
+//      do{
+//         r=x*tabuada;
+//          System.out.printf("%d * %d = %d\n",x,tabuada, r);
+//      tabuada++;
+//      }while(tabuada<=10);
+//-------------------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------------------
+      
+    }
+    
+}
