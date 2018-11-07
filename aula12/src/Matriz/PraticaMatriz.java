b4fdfc05a3efc08c742cbe5a1cefa24191dd3e2b
System Administrator <root@Victors-MacBook-Pro.local>
2018-11-07 01:21:27 -0200

    Décima segunda aula de progrmação orientada à objetos

---
+++ aula12/src/Matriz/PraticaMatriz.java
@@ -0,0 +1,118 @@
+package Matriz;
+import java.util.Scanner;
+public class PraticaMatriz {
+    public static void main(String[] args) {
+        Scanner matriz = new Scanner (System.in);
+//        Integer [][] num = new Integer [2][2];
+//        
+//        for (int linha = 0; linha <= 1; linha++) {
+//            for (int coluna = 0; coluna <= 1; coluna++) {
+//                System.out.println("Digite um valor: ");
+//                num[linha][coluna] = matriz.nextInt();
+//            }
+//        }
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.printf("%d\n",num[i][j]);   
+//            }
+//            
+//        }
+//------------------------------------------------------------------------------------------
+//Exercicio 1:
+//------------------------------------------------------------------------------------------
+//Integer [][] num = new Integer [3][4];
+//int soma =0;
+//        for (int linha = 0; linha < 3 ; linha++) {
+//            for (int coluna = 0; coluna < 4; coluna++) {
+//                System.out.println("Digite um valor: ");
+//                num [linha][coluna] = matriz.nextInt();   
+//            }
+//        }
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.printf("%d ", num[i][j]);
+//            }
+//            System.out.println("");
+//        }
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                 soma += num[i][j];
+//            }
+//            System.out.printf("O valor da somatoria dos valores da matriz é: %d\n",soma);
+//        }
+//------------------------------------------------------------------------------------------
+// Exercicio 2
+//------------------------------------------------------------------------------------------
+//Integer m[][] = { {3, 5, 2, 1,10},
+//                  {15,12,18,4, 6},
+//                  {7,9,13,16, 20}
+//                };
+//        for (int linha = 0; linha < 3; linha++) {
+//            for (int coluna = 0; coluna < 5; coluna++) {
+//                System.out.printf("%d ",m[linha][coluna]);
+//            }
+//            System.out.println("");
+//        }
+//------------------------------------------------------------------------------------------
+// Exercicio 3
+//------------------------------------------------------------------------------------------
+//Integer[][] num = new Integer[3][5];
+//int matrizprincipal=0;
+//
+//
+//            for (int linha = 0; linha < 3; linha++) {
+//                     for (int coluna = 0; coluna < 5; coluna++) {
+//                                System.out.println("Informe um valor: ");
+//                                num[linha][coluna] = matriz.nextInt();
+//                           
+//                                            if (linha == coluna) {
+//                                                matrizprincipal += num [linha][coluna];
+//                                            }
+//                       }
+//            }
+//    
+//            System.out.printf("A soma da Matriz principal é: %d\n",matrizprincipal);
+//        
+//        
+//            for (int i = 0; i < 3; i++) {
+//                     for (int j = 0; j < 5; j++) {
+//                        System.out.printf("%d ",num[i][j]);
+//                }
+//                        System.out.println("");
+//            }
+//------------------------------------------------------------------------------------------
+// Exercício 4
+//------------------------------------------------------------------------------------------
+//float [][] num = new float[3][2];
+//float media=0;
+//float n  = 20.75f ;
+//
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                
+//                System.out.println("Informe um valor: ");
+//                
+//                num[i][j] = matriz.nextFloat();
+//            }
+//        }
+//        
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                media  = num[i][j] ;
+//                    
+//                        if(media > n){
+//                        System.out.println(";-;"+media);
+//                    }
+//            }
+//        }
+//                
+//        
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                System.out.printf(" %.2f ", num[i][j]);
+//            }
+//            System.out.println(" ");
+//        }
+
+    }
+}
